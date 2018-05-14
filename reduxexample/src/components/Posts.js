b46df0ad8aa9af49aad88f2b8c3ca4fa73d05507
John Walker Jr <john_walkerjr@comcast.com>
2018-05-14 11:55:23 -0500

    Fetch posts and display them

--- reduxexample/src/components/Posts.js
+++ reduxexample/src/components/Posts.js
@@ -10,23 +10,37 @@ import React, { Component } from 'react'
 // });
 
 class Posts extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: []
+    }
+  }
 
   // For React 17, move to componentDidMount() or the constructor.
   UNSAFE_componentWillMount(){
     console.log('Posts - Pre-Mount');
     // Fetch posts from jsonplaceholder test json rest api
-    fetch('https://jsonplaceholder.typicode.com/posts/1')
+    fetch('https://jsonplaceholder.typicode.com/posts')
       .then(response => response.json())
-      .then(data => console.log(data))
+      .then( data => {this.setState({posts: data})})
       .catch(error => console.log('Error fetching posts: ' + error))
   }
 
   render() {
+    const postItems = this.state.posts.map( post => (
+      <div key={post.id}>
+        <h3>{post.title}</h3>
+        <p>{post.body}</p>
+      </div>
+    ));
+
     return (
       <div>
         <h1>Posts</h1>
+        {postItems}
       </div>
-    )
+    );
   }
 }
 
