8217fc46f6d6c01a12ea0f9e743538b1a7a69480
John Walker Jr <john_walkerjr@comcast.com>
2018-05-14 11:11:24 -0500

    Fetch posts from jsonplaceholder test api server

--- reduxexample/src/components/Posts.js
+++ reduxexample/src/components/Posts.js
@@ -1,6 +1,26 @@
 import React, { Component } from 'react'
 
+// Using fetch - https://scotch.io/tutorials/how-to-use-the-javascript-fetch-api-to-get-data
+// fetch(url) // Call the fetch function passing the url of the API as a parameter
+// .then(function() {
+//     // Your code for handling the data you get from the API
+// })
+// .catch(function() {
+//     // This is where you run code if the server returns any errors
+// });
+
 class Posts extends Component {
+
+  // For React 17, move to componentDidMount() or the constructor.
+  UNSAFE_componentWillMount(){
+    console.log('Posts - Pre-Mount');
+    // Fetch posts from jsonplaceholder test json rest api
+    fetch('https://jsonplaceholder.typicode.com/posts/1')
+      .then(response => response.json())
+      .then(data => console.log(data))
+      .catch(error => console.log('Error fetching posts: ' + error))
+  }
+
   render() {
     return (
       <div>
