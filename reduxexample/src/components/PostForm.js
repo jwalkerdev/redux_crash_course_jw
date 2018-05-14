37c2728a4944a19fc1c8766374443aa7596b3e29
John Walker Jr <john_walkerjr@comcast.com>
2018-05-14 12:27:47 -0500

    Add onSubmit and POST the data

--- reduxexample/src/components/PostForm.js
+++ reduxexample/src/components/PostForm.js
@@ -8,19 +8,40 @@ class PostForm extends Component {
       body: ''
     }
 
-    // bind methods to this
+    // bind methods to 'this'
     this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
   }
 
   onChange(e) {
     this.setState( { [e.target.name]: e.target.value } );
   }
 
+  onSubmit(e) {
+    e.preventDefault();
+
+    const post = {
+      title: this.state.title,
+      body: this.state.body
+    }
+
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(post)
+    })
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .catch(error => console.log('Error fetching posts: ' + error))
+  }
+
   render() {
     return (
       <div>
         <h1>Add Posts</h1>
-        <form>
+        <form onSubmit={this.onSubmit}>
           <div>
             <label>Title: </label><br />
             <input type="text" name="title" value={this.state.title} 
