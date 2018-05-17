38ff2853dfc6dc02d45df53e6eb8e736a60911ca
John Walker Jr <john_walkerjr@comcast.com>
2018-05-16 21:25:55 -0500

    Fixed minor issues

--- reduxexample/src/components/Posts.js
+++ reduxexample/src/components/Posts.js
@@ -14,20 +14,20 @@ import { fetchPosts } from '../actions/postActions';
 // });
 
 class Posts extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      posts: []
-    }
-  }
 
   // For React 17, move to componentDidMount() or the constructor.
   UNSAFE_componentWillMount(){
     this.props.fetchPosts();
   }
 
+  componentWillReceiveProps(nextProps) {
+    if(nextProps.newPost) {
+      this.props.posts.unshift(nextProps.newPost);
+    }
+  }
+
   render() {
-    const postItems = this.state.posts.map( post => (
+    const postItems = this.props.posts.map( post => (
       <div key={post.id}>
         <h3>{post.title}</h3>
         <p>{post.body}</p>
@@ -45,11 +45,13 @@ class Posts extends Component {
 
 Posts.propTypes = {
   fetchPosts: PropTypes.func.isRequired,
-  posts: PropTypes.array.isRequired
+  posts: PropTypes.array.isRequired,
+  newPost: PropTypes.object
 }
 
 const mapStateToProps = state => ({
-  posts: state.posts.items
+  posts: state.posts.items,
+  newPost: state.posts.item
 })
 
 export default connect(mapStateToProps, { fetchPosts })(Posts);
